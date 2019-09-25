7dd41b255b7b2bc257758f002ec09bd675b0bcc2
Kneeslapper <gloriaklee8@gmail.com>
2019-09-24 22:02:56 -0700

    added .serializeArray to take input and put into an array, then subtracted total to whatever datais.lenth

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,40 +1,51 @@
-//get document ready to push any jquery
+//get document ready to push any jquery\
+
 $(document).ready(function () {
+    var countDown;
+    var time = 90
+    function start() {
+        time--;
+        $('#timer').text(time)
+        if (time === 0) {
+            $('.questions').hide();
+            clearInterval(countDown);
+            console.log(countDown);
+        }
+    }
+
     console.log("ready");
     $('#start').on("click", function () {
-        $('.questions').toggle()
-        $('#start').hide()
-        $('#timer').text(90)
+        $('.questions').toggle();
+        $('#start').hide();
+        $('#timer').text(time);
+        countDown = setInterval(start, 1000);
     })
 
-})
+
+    $("form").on("submit", function (event) {
+        event.preventDefault();
+        //
+        var data = $("form").serializeArray();
+        console.log(data);
+        console.log(9 - data.length)
+        $('.unanswered').text(9 - data.length);
+    });
+
+}
+
+)
 
 
 
 
 
-
-    // //button disappear after it is clicked
-    // $("#start").on('Click' = "this.style.visibility= 'hidden';");
-
-
-
-
-
-
-
-
-
-
-
-//     //more variables, need to add .text to get them to appear in the right/wrong div//
-//     var correct = 0;
-//     $("#right").text(correct)
-//     var incorrect = 0;
-//     $("#wrong".text(incorrect)
-//     //need to make condition to mark unanswered questions as wrong once time runs out//
-//     var unanswered = 0;
-//     var timeStart = 25;
+//more variables, need to add .text to get them to appear in the right/wrong div//
+var correct = 0;
+$("#right").text(correct)
+var incorrect = 0;
+$("#wrong").text(incorrect)
+//need to make condition to mark unanswered questions as wrong once time runs out//
+var unanswered = 0;
 
 
 
